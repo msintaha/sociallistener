a886419d413df5280e8a2dfd02bbb74bc3a881a1
Mifta Sintaha <msintaha94@gmail.com>
2017-01-09 21:00:27 +0600

    Remove twitter access keys

--- livefeed.js
+++ livefeed.js
@@ -1,55 +1,31 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
     var c=Hash.find().count();
-if(c==0){
-    Hash.insert({hashtag:'#Webabulous'});
-  }  
+	if(c==0){
+	    Hash.insert({hashtag:'#Webabulous'});
+	  }  
     var theTag=Hash.find().fetch()[c-1].hashtag;
   
-//Hash.find
-    // code to run on server at startup
-	 var Twit = Meteor.npmRequire('twit');
+	var Twit = Meteor.npmRequire('twit');
 
     var T = new Twit({
-        consumer_key:         'ClkspMmRMOTAJvysssMpylY56', // API key
-        consumer_secret:      '34qxajeFKcPTvMocYT5xMdOTZrQohbOAO1btgpRihJuA1tGxlM', // API secret
-        access_token:         '138086873-jMTpMLwOTPKRRDQ9MnlgUvwIollZTadG3i9ZnRlz', 
-        access_token_secret:  'xFdxY1bV2aRaxvy5AJ16i7kCxsVNe6MzThTr5QgJxJzwJ'
+        consumer_key:         '', // API key
+        consumer_secret:      '', // API secret
+        access_token:      '',
+        access_token_secret:  ''
     });
-console.log(theTag);
-var stream = T.stream('statuses/filter', { track: theTag, language:'en', limit:1});
-stream.on('tweet', Meteor.bindEnvironment(function (tweet) {
 
-  Tweets.insert({
-    tweet:tweet.text,
-    media:tweet.entities,
-    name:tweet.user.name,
-    username:tweet.user.screen_name,
-    userdp:tweet.user.profile_image_url_https
-  });
-}));
+	var stream = T.stream('statuses/filter', { track: theTag, language:'en', limit:1});
+	stream.on('tweet', Meteor.bindEnvironment(function (tweet) {
 
-// var Instagram = Meteor.npmRequire('instagram-node-lib');
-
-// Instagram.set('client_id', 'eb92efd7f42342178f31a0502a04467a');
-// Instagram.set('client_secret', 'a10ceddc9d714afb86d74908607727d9');
-
-// Instagram.set('callback_url', 'https://webfeed.fwd.wf/callback');
-// Instagram.set('redirect_uri', 'https://webfeed.fwd.wf');
-
-// Instagram.tags.recent({ name: 'webabulous',
-//  complete: function(data) {
-//       //console.log(data);
-//       }
-//   });
-
-// Instagram.tags.subscribe({
-//   object_id: 'webable'
-// });
-// Instagram.tags.subscribe({
-//   object_id: 'webabulous'
-// });
-
+  		Tweets.insert({
+			tweet:tweet.text,
+			media:tweet.entities,
+			name:tweet.user.name,
+			username:tweet.user.screen_name,
+			userdp:tweet.user.profile_image_url_https
+  		});
+	}));
 });
   Meteor.methods({
     'removeAlltweets':function(){
